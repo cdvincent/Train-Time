7f459b57cd35de7f7966a25833e03427b4773edc
Chris Vincent <cdvincent89@gmail.com>
2019-10-28 15:19:26 -0400

    form is being pushed to firebase properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDnw6YHbDG5k8CjryY9Go3R7fOgm02JPRo",
-    authDomain: "test-project-df786.firebaseapp.com",
-    databaseURL: "https://test-project-df786.firebaseio.com",
-    projectId: "test-project-df786",
-    storageBucket: "test-project-df786.appspot.com",
-    messagingSenderId: "602971192011",
-    appId: "1:602971192011:web:a4f399b1bcf822e84df94e"
-};
-
+    apiKey: "AIzaSyCg0izlNPPwhzL9W40oyKP9bduiTE9qUow",
+    authDomain: "train-time-bf7e1.firebaseapp.com",
+    databaseURL: "https://train-time-bf7e1.firebaseio.com",
+    projectId: "train-time-bf7e1",
+    storageBucket: "train-time-bf7e1.appspot.com",
+    messagingSenderId: "606372764875",
+    appId: "1:606372764875:web:b7027904207ee82bf8cf4e"
+  };
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database()
 
@@ -27,4 +26,17 @@ $("#submit").on("click", function(event) {
     $("#trainTime").val("");
     $("#frequency").val("");
 
+    database.ref().push({
+        TrainName: trainName,
+        Destination: destination,
+        TrainTime: trainTime,
+        Frequency: frequency,
+    })
 });
+
+database.ref().on("child_added", function(snapshot) {
+    trainName = snapshot.child("TrainName").val();
+    destination = snapshot.child("Destination").val();
+    trainTime = snapshot.child("TrainTime").val();
+    frequency = snapshot.child("Frequency").val();
+})
