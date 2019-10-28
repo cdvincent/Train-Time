c7154ba6b1db238f949f9d36dc0f917133e78400
Chris Vincent <cdvincent89@gmail.com>
2019-10-28 13:06:01 -0400

    added train schedule and form

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDnw6YHbDG5k8CjryY9Go3R7fOgm02JPRo",
+    authDomain: "test-project-df786.firebaseapp.com",
+    databaseURL: "https://test-project-df786.firebaseio.com",
+    projectId: "test-project-df786",
+    storageBucket: "test-project-df786.appspot.com",
+    messagingSenderId: "602971192011",
+    appId: "1:602971192011:web:a4f399b1bcf822e84df94e"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database()
+
